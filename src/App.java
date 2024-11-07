4f6bbe6a7ab78ed49bc9fccc497f0322c05b59a9
Zümre Güven <zumreguvenn@gmail.com>
2024-11-07 15:39:05 +0300

    2.commit işlemidir. GET isteği yapan yeni bir enpoint yazılması.Bu commit iki sayiyi alir ve toplamını yazdirir.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("***********");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Lütfen ilk sayiyi giriniz  ");
+        int sayi1 = scanner.nextInt();  // İlk sayıyı alıyoruz
+        
+        System.out.println("Lütfen ikinci sayiyi giriniz ");
+        int sayi2 = scanner.nextInt();  // İkinci sayıyı alıyoruz
+        
+        // getSum metodunu çağırarak sonucu yazdırıyoruz
+        System.out.println("The sum of " + sayi1 + " and " + sayi2 + " is: " + getSum(sayi1, sayi2));
+        
+        
+        
+    }
+
+    public static String getSum(int num1,int num2) {
+        int sum=num1+num2;
+        return "2.commit işelmi olarak iki sayının toplamı : Toplama : " + sum;
     }
 }
