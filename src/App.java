784e90957081d30bb3176488b8c749333db86f44
Zümre Güven <zumreguvenn@gmail.com>
2024-11-07 15:25:19 +0300

    yazılımMühendisliğinde Gelişmeler1.comit

--- src/App.java
+++ src/App.java
@@ -1,16 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        getSum();
-    }
-
-    public String getSum() {
-        // Sabit olarak 2 sayısını topluyoruz
-        int number1 = 2;
-        int number2 = 2;
-        int sum = number1 + number2;
-
-        // Sonucu döndürüyoruz
-        return "The sum of 2 and 2 is: " + sum;
     }
 }
